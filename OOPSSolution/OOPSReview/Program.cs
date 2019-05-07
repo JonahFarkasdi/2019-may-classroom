44ac973ed368ed2c98556e025e7fe930c7ed2c62
JonahFarkasdi <50343707+JonahFarkasdi@users.noreply.github.com>
2019-05-06 18:47:20 -0600

    create my first class

--- OOPSSolution/OOPSReview/Program.cs
+++ OOPSSolution/OOPSReview/Program.cs
@@ -11,6 +11,21 @@ namespace OOPSReview
         static void Main(string[] args)
         {
             Console.WriteLine("Hello");
-        }
-    }
-}
+
+            My_Data aVariable;
+
+            My_Data anInstance = new My_Data();
+
+            if(aVariable == null)
+            {
+                Console.WriteLine("aVariable has nothing in it");
+            }
+
+            if(anInstance == null)
+            {
+                Console.WriteLine("anInstance has nothing in it");
+            }
+            Console.ReadLine();
+        }//main
+    }//class program
+}//namespace
