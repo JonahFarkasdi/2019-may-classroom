931c51b6b65efa0af19de2fc69c8f865044943a5
JonahFarkasdi <50343707+JonahFarkasdi@users.noreply.github.com>
2019-05-06 18:19:33 -0600

    Update Program.cs

--- OOPSSolution/OOPSReview/Program.cs
+++ OOPSSolution/OOPSReview/Program.cs
@@ -10,7 +10,7 @@ namespace OOPSReview
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("hello");
+            Console.WriteLine("Hello");
         }
     }
 }
